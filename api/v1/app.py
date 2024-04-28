c80fddc120f974e0506aa0017f088783021a9c09
MicrQ <mail.micrq@proton.me>
2024-04-28 14:39:11 +0300

    errorhandler implemented for 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ a flask app """
 
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 from os import getenv
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_storage(self):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
